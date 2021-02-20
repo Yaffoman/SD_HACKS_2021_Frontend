9a68e96da803ad16ab6f47cdcba987d023dced6d
Amit Bar <amitbar00@gmail.com>
2021-02-20 11:25:18 -0800

    created a ton of initial stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar';
+
+function Routes() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='./' component={Home} />
+      </Switch>
+    </Router>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Routes />
     </div>
   );
 }
